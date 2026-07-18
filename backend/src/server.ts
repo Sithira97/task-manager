456c91737ba94cfc6cb1e1fcc5365d58b429fc17
Sithira-Mac <sithindu@gmail.com>
2026-07-18 19:07:44 +0530

    fix: missing host variable

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,9 @@ import app from "./app.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 3001;
-const host = process.env.HOST || "localhost"
+const port = Number(process.env.PORT) || 3001;
+const host = process.env.HOST || "localhost";
 
-app.listen(port, () => {
-  console.log(`Task Manager API Server running on http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Task Manager API Server running on http://${host}:${port}`);
 });
